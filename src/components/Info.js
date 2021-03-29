0a027abd050923c427f32d81bb612a394394fc19
wishiknew <wishiknew5@gmail.com>
2021-03-29 23:43:28 +0530

    changes the font

--- src/components/Info.js
+++ src/components/Info.js
@@ -2,9 +2,9 @@ export default function Info ({user}){
   return (
     <div className='info'>
       <h2>{user.name}</h2>
-      <h3><i class="fas fa-envelope"></i><a href={`mailto://${user.email}`}>{user.email}</a></h3>
-      <h3><i class="fas fa-phone"></i>{user.phone}</h3>
-      <h3><i class="fas fa-globe"></i><a href={user.website}>{user.website}</a></h3>
+      <h3><i class="fal fa-envelope"></i><a href={`mailto://${user.email}`}>{user.email}</a></h3>
+      <h3><i class="fal fa-phone"></i>{user.phone}</h3>
+      <h3><i class="fal fa-globe"></i><a href={user.website}>{user.website}</a></h3>
     </div>
   )
 }
